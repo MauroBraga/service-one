62d06cde3dafaad94e324328666fc5e351ccd0b5
mauro <mrb0305@gmail.com>
2025-05-26 20:49:21 -0300

    service one

---
+++ src/main/java/br/com/mrb/serviceone/HelloWorldController.java
@@ -0,0 +1,17 @@
+package br.com.mrb.serviceone;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @Value("${message:Default Hello}")
+    private String message;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return message;
+    }
+}
